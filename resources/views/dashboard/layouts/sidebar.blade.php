9b264b7f34cb9364c96246b8017575729b449b20
Saputra <saputra.uta50@gmail.com>
2024-01-09 13:48:03 +0800

    Post Controller

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -1,11 +1,11 @@
         <aside class="w-64 bg-slate-700 text-slate-100">
             <ul class="list-none flex flex-col mt-10 w-full">
                 <li class="text-xl font-medium text-slate-100 hover:bg-slate-800 hover:cursor-pointer p-4"><a
-                        href="#"
+                        href="/dashboard"
                         class="flex items-center gap-1"><span>@include('feather::home')</span>Dashboard</a></li>
                 </li>
                 <li class="text-xl font-medium text-slate-100 hover:bg-slate-800 hover:cursor-pointer p-4"><a
-                        href="#" class="flex items-center gap-1"><span>@include('feather::file-text')</span>My
+                        href="/dashboard/posts" class="flex items-center gap-1"><span>@include('feather::file-text')</span>My
                         Posts</a></li>
             </ul>
         </aside>
