468bb4ec13b6cd2f6d5cd4f12d357d02d453eede
Saputra <saputra.uta50@gmail.com>
2024-01-12 17:30:39 +0800

    Responsive login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -9,7 +9,7 @@
     @vite('resources/css/app.css')
 </head>
 
-<body class="bg-slate-100 min-h-screen flex flex-col justify-center">
+<body class="bg-slate-200 min-h-screen flex flex-col justify-center gap-12">
     @if (session()->has('success'))
         <small class="block text-center text-lg text-green-500 tracking-wider">
             {{ session('success') }}
@@ -21,12 +21,12 @@
             {{ session('loginError') }}
         </small>
     @endif
-    <h2 class="text-2xl font-bold tracking-wide text-center">Please Login First</h2>
-    <main class="mt-10 flex items-center justify-center">
-        <form action="/login" method="post" class="max-w-xl flex flex-col w-full">
+    <h2 class="text-3xl font-bold text-center text-slate-700">Please Login First</h2>
+    <main class="flex items-center justify-center">
+        <form action="/login" method="post" class="max-w-xl flex flex-col w-3/4">
             @csrf
             <div class="flex flex-col">
-                <label for="email" class="font-medium text-slate-700">Email</label>
+                <label for="email" class="font-medium text-slate-700 sm:text-lg md:text-xl">Email</label>
                 <input type="email" name="email"
                     class="text-slate-700 border-slate-900 rounded-lg p-1 focus:outline-none" autofocus required
                     value="{{ old('email') }}" />
@@ -35,16 +35,16 @@
                 @enderror
             </div>
             <div class="flex flex-col mt-2">
-                <label for="password" class="font-medium text-slate-700">Password</label>
+                <label for="password" class="font-medium text-slate-700 sm:text-lg md:text-xl">Password</label>
                 <input type="password" name="password"
                     class="text-slate-700 border-slate-900 rounded-lg p-1 focus:outline-none" required />
             </div>
             <button type="submit"
-                class="mt-8 w-full bg-slate-700 text-slate-100 rounded-lg p-2 font-medium hover:bg-slate-900">Login</button>
-            <small class="block text-center mt-2 text-base">Don't have any account yet? <a href="/register"
-                    class="underline">register here</a></small>
+                class="sm:text-lg md:text-xl mt-8 w-full bg-slate-700 text-slate-100 rounded-lg p-2 font-bold hover:bg-slate-900">Login</button>
         </form>
     </main>
+    <small class="block text-center mt-2 text-base">Don't have any account yet? <a href="/register"
+            class="underline">register here</a></small>
 </body>
 
 </html>
